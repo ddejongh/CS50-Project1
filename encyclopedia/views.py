6a917d6e90899610117ace757f931247419c5807
Devin DeJongh <devindejongh@gmail.com>
2020-12-16 23:24:48 -0600

    wip first entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,6 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# add a title function here 
+# def title(request, title):
+#     return render(request, "encyclopedia/index.html")
