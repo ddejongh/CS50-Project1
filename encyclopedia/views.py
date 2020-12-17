5a425f82218eca2f910d2c04fc82f54f2d954c51
Devin DeJongh <devindejongh@gmail.com>
2020-12-17 01:20:00 -0600

    testing minor changes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from encyclopedia.util import get_entry
 
 from . import util
 
@@ -11,3 +12,11 @@ def index(request):
 # add a title function here 
 # def title(request, title):
 #     return render(request, "encyclopedia/index.html")
+
+# okay so here we need to refamiliarize ourselves with the md syntax
+# this is essentially the hello greet thing from the lecture
+# we just need to figure out a way to pull the md files 
+def title(request, title):
+    return render(request, "encyclopedia/index.html", {
+        "entry" : get_entry(title)
+    })
