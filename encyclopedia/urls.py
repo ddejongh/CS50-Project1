13ccc19a521406db17ce25d6539464e2fe33df7b
Devin DeJongh <devindejongh@gmail.com>
2020-12-18 11:14:45 -0600

    temp

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.title, name="title") # this seems to be working
+    path("wiki/<str:title>", views.title, name="title") # this seems to be working , still gets everything though
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,20 +3,11 @@ from encyclopedia.util import get_entry
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-# add a title function here 
-# def title(request, title):
-#     return render(request, "encyclopedia/index.html")
-
-# okay so here we need to refamiliarize ourselves with the md syntax
-# this is essentially the hello greet thing from the lecture
-# we just need to figure out a way to pull the md files 
 def title(request, title):
-    return render(request, "encyclopedia/index.html", {
-        "entry" : get_entry(title)
-    })
+    return render(request, "encyclopedia/index.html") # now we inject our title to the page somehow 
+    # make sure it checks if title exists, if not display missing page or whatever 
